050be828dbb3d09ec7a887342ad660b738da4ce5
jinsookim <108246955+jinsoo96@users.noreply.github.com>
2022-10-27 14:53:03 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 1.88 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -0,0 +1,23 @@
+def getHead(file):    
+    res = ""   
+    for c in file:        
+        if c.isdigit():            
+            break        
+        else:            
+            res += c    
+    return res.lower() 
+
+def getNumber(file):   
+    res = ""    
+    isTail = False    
+    for c in file:        
+        if c.isdigit():            
+            isTail = True           
+            res += c        
+        elif isTail:           
+                break    
+    return int(res) 
+
+def solution(files):    
+    answer = sorted(files, key = lambda x : (getHead(x), getNumber(x)))    
+    return answer
