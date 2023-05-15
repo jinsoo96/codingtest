f90da03314d999a995f9809b51bedafd622c8d8d
jinsookim <108246955+jinsoo96@users.noreply.github.com>
2023-05-15 15:54:52 +0900

    [unrated] Title: 2의 영역, Time: 0.93 ms, Memory: 15.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181894. 2의 영역/2의 영역.py
@@ -0,0 +1,8 @@
+def solution(arr):
+    if 2 not in arr:
+        return [-1]
+    else:
+        first_index = arr.index(2)
+        last_index = len(arr) - 1 - arr[::-1].index(2)
+        answer = arr[first_index:last_index+1]
+        return answer
