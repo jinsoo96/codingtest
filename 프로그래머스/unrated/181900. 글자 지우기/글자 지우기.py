7c8e1b72ecf052e53f75955b47ca9f51992819bc
jinsookim <108246955+jinsoo96@users.noreply.github.com>
2023-05-19 15:22:43 +0900

    [unrated] Title: 글자 지우기, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.py
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.py
@@ -1,6 +1,6 @@
-def solution(my_string, indices):
-    answer = ''
-    for i in range(len(my_string)):
-        if i  not in indices:
-            answer += my_string[i]
-    return answer
+def solution(my_string,indices):
+   answer =''
+   for i,n in enumerate(my_string):
+      if  i not in indices:
+         answer += my_string[i]
+   return answer
