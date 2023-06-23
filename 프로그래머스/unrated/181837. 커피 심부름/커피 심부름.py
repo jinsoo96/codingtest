b2139f2402294e77634e43d14a22cfc2fdb3deb0
jinsookim <108246955+jinsoo96@users.noreply.github.com>
2023-06-23 14:49:55 +0900

    [unrated] Title: 커피 심부름, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.py
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.py
@@ -1,18 +1,12 @@
 def solution(order):
-    menu = {
-        "iceamericano": 4500, 
-        "americanoice": 4500, 
-        "hotamericano": 4500, 
-        "americanohot": 4500, 
-        "icecafelatte": 5000, 
-        "cafelatteice": 5000, 
-        "hotcafelatte": 5000, 
-        "cafelattehot": 5000, 
-        "americano": 4500, 
-        "cafelatte": 5000, 
-        "anything": 4500
-    }
-    total_price = 0
-    for o in order:
-        total_price += menu[o]
-    return total_price
+    answer = 0
+
+    for i in order:
+      if "americano" in i:
+        answer += 4500
+      elif "cafelatte" in i:
+        answer += 5000
+      else:
+        answer += 4500
+
+    return answer
